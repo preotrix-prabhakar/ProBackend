d041fdc1c4e03a946b768524ea2a3ec5eaeeb583
preotrix-prabhakar <prabhakar8859@gmail.com>
2024-10-22 15:41:24 +0530

    update name and password complete

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,23 +2,19 @@ const dotenv=require("dotenv");
 const jwt=require("jsonwebtoken");
 dotenv.config();
 const authMidlleware=async (req,res,next)=>{
-    const token = req.header("Authorization");
+    const authHeader = req.header("Authorization");
+    const token = authHeader && authHeader.split(' ')[1];
     if(!token){
       return  res.status(400).json({message:`acess denied or user is not logged in`});
     }
 
     try {
         const decoded=jwt.verify(token,process.env.SECRET_KEY);
-        req.user=decoded.id;
-        if(decoded){
-            return res.status(200).json({message:`user verified successfully`,decoded});
-        }
-        else{
-            return res.status(400).json({message:`something went wrong`});
-        }
+        req.user=decoded;
         next();
     } catch (error) {
         console.log(`error verifying user`,error);
+        return res.status(403).json({message:`Invalid token`});
     }
 }
 
--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const bcrypt=require("bcrypt");
 const jwt=require("jsonwebtoken");
 const dotenv=require("dotenv");
 const router=express.Router()
-
+const authMidlleware=require("../middleware/auth")
 dotenv.config();
 
 router.get('/',(req,res)=>{
@@ -29,44 +29,17 @@ router.post('/register',async (req,res)=>{
         // password:password,
         password:hashedPassword
     })
-    
     const user=await newUser.save()
-       
     res.status(200).json({user});
-    console.log(user);
+    console.log(user); 
  
 })
-function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]; 
-
-    if (!token) {
-        return res.sendStatus(401); 
-    }
-
-    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
-        if (err) {
-            return res.sendStatus(403); 
-        }
-
-        
-        req.user = {
-            id: decoded.id,
-            email: decoded.email
-        };
-        
-        next(); 
-    });
-}
-
-
-
-router.get('/checkRoute', authenticateToken, (req, res) => {
+router.get('/checkRoute', authMidlleware, (req, res) => {
     res.json({ message: "This is a protected route",  user: {
         id: req.user.id,
         email: req.user.email
     }});
-});
+}); 
 router.post('/login',async (req,res)=>{
     const {email,password}=req.body;
     if(!(email || password)){return res.status(400).json({message:`both fields are necessary`})}
@@ -79,16 +52,59 @@ router.post('/login',async (req,res)=>{
     if(!validPass){
         res.status(401).json({message:`invalid credentials`});
     }
-    let token = await jwt.sign({id: user.id, email: user.email}, process.env.SECRET_KEY);
+    let token = await jwt.sign({id: user.id}, process.env.SECRET_KEY);
     res.header('Authorization', `Bearer ${token}`).json({
         message: 'Logged in successfully',
         token: token
     });
-    // res.json({message: "login successful",jwt: token});
     console.log(validPass);
     console.log(token);
 }) 
+router.patch('/update/password',authMidlleware,async (req,res)=>{
+    try {
+        const {newPassword,oldPassword}=req.body;
+        if(!newPassword || !oldPassword){
+            return res.status(400).json({message:"Bad Request"});
+        }
+        const userId=req.user.id;
+        const userDetails=await userModel.findOne({_id:userId});
+        console.log(userDetails.password,oldPassword,newPassword);
 
+        let isCorrectPass= await bcrypt.compare(oldPassword,userDetails.password);
+        console.log("asdf",isCorrectPass);
+        
+        if(!isCorrectPass){return res.json({error:"invalid credentials"})}
+
+        let newHashedPass=await bcrypt.hash(newPassword,10);
+
+        await userModel.updateOne({_id:userId},{$set:{password:newHashedPass}});
+        res.status(200).json({message:'password reset successfullly'});
+    } catch (error) {
+     console.log(error);
+     res.status(500).json({message:"Internal server Error"});   
+    } 
+})
+router.patch('/update/name',authMidlleware,async (req,res)=>{
+
+        try{
+          let {name}=req.body;
+          if(!name){
+            return res.status(400).json({ error: "bad request" });
+          }
+          const userId = req.user.id;
+          await userModel.updateOne(
+            { _id: userId },
+            { $set: { name } }
+          );
+      
+          res.status(200).json({message:"name ad set successfully"});
+      
+        }
+        catch(error){
+          console.log(error);
+        }
+
+})
 
 module.exports=router;
 // $2b$10$ul9F0I8Uf04rAMOymu75Ie4DZeld.mjxWwHKOpLa9r8ROn90cP6ca  
